591e17eb79bde1ead27c1db4645014fa7f07d4ca
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-07-18 05:18:34 -0600

    update css in pizza-menu

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { data } from "./data.js";
+import "./index.css";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <h1>Hello React</h1>
       <Header />
       <Menu />
@@ -14,17 +15,67 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 
-function Menu() {
+function Menu(data) {
   return (
     <div>
-      <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <main className="menu">
+        <h2>Our menu</h2>
+        <Pizza
+          name="Pizza Spinaci"
+          ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+          photoName="pizzas/spinaci.jpg"
+          price={10}
+        />
+        <Pizza
+          name="Pizza Focaccia"
+          ingredients="Bread with italian olive oil and rosemary"
+          photoName="pizzas/focaccia.jpg"
+          price={8}
+        />
+        <Pizza
+          name="Pizza Funghi"
+          ingredients="Tomato, mozarella, mushrooms, and onion"
+          photoName="pizzas/funghi.jpg"
+          price={15}
+        />
+        <Pizza
+          name="Pizza Margherita"
+          ingredients="Tomato and mozarella"
+          photoName="pizzas/margherita.jpg"
+          price={12}
+        />
+        <Pizza
+          name="Pizza Salamino"
+          ingredients="Tomato, mozarella, and pepperoni"
+          photoName="pizzas/salamino.jpg"
+          price={14}
+        />
+        <Pizza
+          name="Pizza Prosciutto"
+          ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+          photoName="pizzas/prosciutto.jpg"
+          price={17}
+        />
+      </main>
+    </div>
+  );
+}
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
 }
@@ -39,17 +90,9 @@ function Footer() {
   //   if (hour >= openHour && hour <= closeHour) alert("We are currently open!");
   //   else alert("Sorry we are closed!");
   return (
-    <footer>{new Date().toLocaleTimeString()}.We are currently opoen!</footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h2>Pizza</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}.We are currently open!
+    </footer>
   );
 }
 
