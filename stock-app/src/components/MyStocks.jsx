390a2db77a7c405a17c920cbe5282fd3ac8f0864
FarhadIbrahimov <farhadibrahimov@gmail.com>
2023-07-20 18:08:11 -0600

    updated

--- stock-app/src/components/MyStocks.jsx
+++ stock-app/src/components/MyStocks.jsx
@@ -1,60 +1,9 @@
 import { Link } from "react-router-dom";
 import { useStockContext } from "./StockProvider";
+import Utils from "./utils";
 
 export default function MyStocks() {
   const { myStock, setMyStock } = useStockContext();
   console.log(myStock);
-  return (
-    <div>
-      <h1 className="h1">My Followed Stocks</h1>
-      <div className="outerContainer">
-        <div className="companyName">
-          Company Name
-          <hr className="tagLine" />
-          {myStock.map((stock, index) => {
-            return (
-              <div key={stock.symbol}>
-                {index !== 0 && <hr />}
-                <Link to={`/stocks/${stock.symbol}`}>
-                  <div>{stock.name}</div>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-        <div className="price">
-          Price
-          <hr className="tagLine" />
-          {myStock.map((stock, index) => {
-            return (
-              <div key={stock.symbol}>
-                {index !== 0 && <hr />}
-                <div>{stock.lastPrice}</div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="change">
-          Change
-          <hr className="tagLine" />
-          {myStock.map((stock, index) => {
-            let stockPrice = (stock.lastPrice - stock.open).toFixed(2);
-            let percent = (stock.change * 100).toFixed(2);
-            return (
-              <div key={stock.symbol}>
-                {index !== 0 && <hr />}
-                <div>
-                  <span style={{ color: stock.change > 0 ? "green" : "red" }}>
-                    {stock.change > 0 ? "+" : null}
-                    {stockPrice} ({stock.change > 0 ? "+" : null}
-                    {percent}%)
-                  </span>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+  return <Utils stocksData={myStock} pageName={"Followed Stocks"} />;
 }
